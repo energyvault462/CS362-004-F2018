02a33c192b34b7abe0df5bf69bfb85c9cac2a794
brent johnson <brentj433@gmail.com>
2018-11-08 18:25:02 -0800

    initial commit

---
+++ projects/johnbre4/dominion/randomtestcard1.c
@@ -0,0 +1,16 @@
+/*
+test written by Brent Johnson
+OSU CS362-400 FALL 2018
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "dominion.h"
+#include "rngs.h"
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
